d5af0a11cc44f3282b926668d8a07e8f7b3fbeae
이지수 <0801ljs@naver.com>
2025-09-22 23:13:11 +0900

    [REFACT] IT/과학 섹션 주소로 URL 변경

--- HW1/scraper.py
+++ HW1/scraper.py
@@ -5,7 +5,7 @@ def scrape_naver_news():
     """
     네이버 뉴스 IT/과학 섹션에서 가장 많이 본 뉴스 상위 10개의 헤드라인과 링크를 스크래핑합니다.
     """
-    URL = 'https://news.naver.com/main/ranking/popularDay.naver?rankingType=popular_day&sectionId=105'
+    URL = 'https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=105'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
@@ -19,18 +19,12 @@ def scrape_naver_news():
 
     soup = BeautifulSoup(response.text, 'html.parser')
     
-    news_list = soup.select('.rankingnews_list > li')
+    news_list = soup.select('.sa_text_title')
     
     headlines = []
     for item in news_list[:10]:
-        anchor = item.select_one('.list_title')
-        if anchor and anchor.has_attr('href'):
-            title = anchor.get_text(strip=True)
-            link = anchor['href']
-            
-            if link.startswith('/main/ranking/'):
-                link = "https://news.naver.com" + link
-            
-            headlines.append({'title': title, 'link': link})
+        title = item.get_text(strip=True)
+        link = item['href']
+        headlines.append({'title': title, 'link': link})
             
     return headlines
